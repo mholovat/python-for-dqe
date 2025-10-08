ce10baec860ca4b390caf1d2d5add4a0a40b430f
Mykhailo_Holovatiuk <Mykhailo_Holovatiuk@epam.com>
2025-10-08 15:23:53 +0300

    updated

--- module_4_1.py
+++ module_4_1.py
@@ -1,1 +1,69 @@
+# Importing random to generate random numbers and letters
+import random
 
+# Importing string to easily access the alphabet
+import string
+
+# Generate a random number of dicts (2 to 10)
+number_of_dicts = random.randint(2, 10)
+
+# Initialise list to store dictionaries
+dicts_list = []
+
+def populate_dictionary(num_keys):
+    # Initialise empty dict within loop iteration
+    dict_object = {}
+
+    # Initialise counter for dict
+    j = 0
+
+    # Populate dictionary with random key-value pairs
+    while j < num_keys:
+        # Randomly choose a lowercase letter for the key
+        key = random.choice(string.ascii_letters).lower()
+
+        # Check if the key already exists
+        if not dict_object.get(key):
+            pass
+
+        # Randomly choose a number (0-100) for the value
+        value = random.randint(1, 100)
+        # Add the key-value pair to the dictionary
+        dict_object[key] = value
+
+        # Increment counter
+        j += 1
+
+    return dict_object
+
+
+def merge_dictionaries(dictionaries_list):
+    # Initialise merged dict
+
+    merged_dict = {}
+
+    for dict in dictionaries_list:
+        # Iterate over dictionary
+        for key, value in dict.items():
+            # Check bigger value for a key, if key already was retrieved from one of the previous dicts
+            if key in merged_dict:
+                if value > merged_dict[key]:
+                    merged_dict[key] = value
+
+            # If the key does not exist in merged dict - add it
+            else:
+                merged_dict[key] = value
+
+    return merged_dict
+
+
+# Populate dicts
+for i in range(number_of_dicts):
+    # Generate a random number of keys for each dict (1 to 26 based on English alphabet)
+    number_keys = random.randint(1, 26)
+
+    # Append the dictionary to the list
+    dicts_list.append(populate_dictionary(number_keys))
+
+
+print(merge_dictionaries(dicts_list))
--- module_4_2.py
+++ module_4_2.py
@@ -1,1 +1,73 @@
+def normalize_string(input_str):
+    return input_str.lower().replace(" iz ", " is ")
 
+def collect_last_words(sentences):
+    last_words = []
+
+    # Iterate over all sentences to get last words
+    for sentence in sentences:
+        last_words.append(sentence.split(' ')[-1])
+
+    # Make sure that the first word in the list of last words is capitalized
+    last_words[0] = last_words[0].capitalize()
+
+    return ' '.join(last_words).rstrip(' ')
+
+def format_sentences(input_str):
+    # Initialize list with sentences
+    sentences = []
+
+    # Iterate over all sentences in the normalized text
+    for sentence in input_str.split('.'):
+
+        # Convert sentence to list as string is immutable and we need to manipulate with it's elements
+        sentence_list = list(sentence)
+
+        # Iterate over characters
+        for i in range(len(sentence_list) - 1):
+
+            # If the symbol is alphanumeric (numeric needed to cover cases when sentence starts with number
+            # convert it to uppercase and exit the loop
+            if sentence_list[i].isalnum():
+                sentence_list[i] = sentence_list[i].upper()
+                break
+
+        # Add the formatted sentence to the list of sentences
+        sentences.append(''.join(sentence_list))
+
+    return sentences
+
+def count_spaces(input_str):
+    # Initialize counter for whitespaces
+    count_sp = 0
+
+    # Iterate over characters in the final text and increase counter when whitespace is identified
+    for char in final_text:
+        if char.isspace():
+            count_sp += 1
+
+    return count_sp
+
+str = """homEwork:
+tHis iz your homeWork, copy these Text to variable.
+
+You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+
+it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
+
+last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
+
+
+
+# Normalize text by putting all letters to lowercase and replace word iz with is
+normalised_str = normalize_string(str)
+
+# Initialize list with sentences
+sentences = format_sentences(normalised_str)
+
+# Merge all sentences into the final text, including the last words
+final_text = '.'.join(sentences) + collect_last_words(sentences) + '.'
+
+# Print the results
+print("Normalized text is: ", final_text)
+print("Number of spaces is: ", count_spaces(final_text))
