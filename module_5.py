0c291a9bf5f5d0b3fbe20ced9ed0ffe3fb2cf259
Mykhailo_Holovatiuk <Mykhailo_Holovatiuk@epam.com>
2025-10-09 16:48:13 +0300

    updated

---
+++ module_5.py
@@ -0,0 +1,70 @@
+from datetime import date, datetime
+
+
+class Publication:
+    def __init__(self, pub_text, pub_type='Publication'):
+        self.pub_text = pub_text
+        self.pub_type = pub_type
+
+    def publish(self,filename):
+        with open(filename,'w') as f:
+            f.write(self.pub_type + '==========\n')
+            f.write(self.pub_text + '\n\n\n')
+
+
+class News(Publication):
+    def __init__(self, pub_text, news_city, date=date.today() ,pub_type='News'):
+        super().__init__(pub_text, pub_type)
+        self.news_city = news_city
+        self.date = date
+
+    def publish(self,filename):
+        with open(filename,'w') as f:
+            f.write(self.pub_type + '==========\n')
+            f.write(self.pub_text + '\n')
+            f.write(self.news_city + '====' + str(self.date) + '\n\n\n')
+
+class PrivateAd(Publication):
+    def __init__(self, pub_text, expiration_date ,pub_type='PrivateAd'):
+        super().__init__(pub_text, pub_type)
+        self.expiration_date = expiration_date
+
+    def publish(self,filename):
+        with open(filename,'w') as f:
+            f.write(self.pub_type + '==========\n')
+            f.write(self.pub_text + '\n')
+            f.write('Days left: ' + str((datetime.strptime(self.expiration_date,'%Y-%m-%d').date() - date.today()).days) + '\n\n\n')
+
+
+class BirthdayWish(Publication):
+    def __init__(self, pub_text, name, pub_type='BirthdayWish'):
+        super().__init__(pub_text, pub_type)
+        self.name = name
+
+    def publish(self,filename):
+        with open(filename,'w') as f:
+            f.write(self.pub_type + '==========\n')
+            f.write(self.name + '\n')
+            f.write(self.pub_text + '\n\n\n')
+
+filename = '/Users/Mykhailo_Holovatiuk/Downloads/publication.txt'
+
+while 1==1:
+    publication_type = input("Please provide the publication type out of the list: News, PrivateAd, BirthdayWish: ")
+    pub_text = input("Please provide your publication text: ")
+
+    if publication_type.lower() == 'news' or publication_type.lower() == 'new':
+        news_city = input("Please provide your city: ")
+        news = News(pub_text, news_city)
+        news.publish(filename)
+    elif publication_type.lower() == 'privatead':
+        expiration_date = input("Please provide expiration date: ")
+        privatead = PrivateAd(pub_text, expiration_date)
+        privatead.publish(filename)
+    elif publication_type.lower() == 'birthdaywish':
+        name = input("Please provide your name: ")
+        birthdaywish = BirthdayWish(pub_text, name)
+        birthdaywish.publish(filename)
+
+    if input("Please click Y if you would like to complete").lower() == 'y':
+        break
